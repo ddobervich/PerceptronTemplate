17e43b4f91669a4128eda8d2a5f9efc43aee75bc
David Dobervich <david.dobervich@gmail.com>
2025-05-14 09:22:17 -0700

    re-order some lines

--- src/TesterClasses/TestPerceptron.java
+++ src/TesterClasses/TestPerceptron.java
@@ -6,9 +6,10 @@ import Perceptron.Perceptron;
 
 public class TestPerceptron {
     public static final String WHAT_TO_CLASSIFY = "virginica";
-    public static final String TRAINING_DATA_FILE = "data/iris.data";
     // all options:  "sepal length", "sepal width", "petal length", "petal width"
     public static final String[] features = {"sepal length", "sepal width"};    // <--- u change it!
+
+    public static final String TRAINING_DATA_FILE = "data/iris.data";
 
     public static void main(String[] args) {
         DataSet dataset;
